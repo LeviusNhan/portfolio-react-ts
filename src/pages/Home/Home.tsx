a89a7f45005cfe690322ded0c912243ef46e2d6d
LeviusNhan <96242199+LeviusNhan@users.noreply.github.com>
2026-07-12 21:05:26 +0700

    chore: setup project structure

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-purple-500">
+        Tailwind v4 Working by levius 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
